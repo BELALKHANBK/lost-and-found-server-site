700ff778e87f3ab636379ae0073fa7fa10c220e5
BELALKHANBK <belalkhanloverboy@gmail.com>
2025-06-10 01:52:22 +0600

    four commit

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
+const logger = (req, res, next) => {
+    console.log('inside the logger middlewer')
+    next()
+}
+const cookie = (req, res, next) => {
+    const token = req.cookies.token; //  এটা আগে define করো
+    console.log('cookie in the middleware', token)
+
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+
+        req.decoded = decoded; //  decoded user info req.user এ রাখো
+        next(); //  সব কিছু ঠিক থাকলে পরবর্তী middleware বা route এ যাও
+    });
+};
+
 
 // MongoDB Connection URI
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@cluster0.eoybt2t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,9 +57,27 @@ async function run() {
         const itemCollection = client.db('lost&found').collection('items');
         const recoveredCollection = client.db('lost&found').collection('recovered');
 
+        //jsonwebtoken
+        app.post('/jwt', async(req, res) => {
+            const user = req.body
+                //token created
+            const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
+                expiresIn: '7d'
+            })
+            console.log(token)
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false,
+                sameSite: 'Lax',
+
+            })
+            res.send({ success: true })
+        })
+
         //  Get all items or filter by user's email
-        app.get('/items', async(req, res) => {
+        app.get('/items', cookie, async(req, res) => {
             const userEmail = req.query.email;
+            console.log('insite lost and found', req.cookies)
             const query = userEmail ? { userEmail } : {};
             const items = await itemCollection.find(query).toArray();
             res.send(items);
@@ -94,7 +140,7 @@ async function run() {
         });
 
         //  Get recovered items by user email
-        app.get('/recovered', async(req, res) => {
+        app.get('/recovered', cookie, async(req, res) => {
             const email = req.query.email;
             const query = { "recoveredBy.email": email };
             const items = await recoveredCollection.find(query).toArray();
