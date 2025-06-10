3ba11893a74d5e23aa0b36aaa1ec9faf4574cfa8
BELALKHANBK <belalkhanloverboy@gmail.com>
2025-06-10 14:43:28 +0600

    five commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken') //jwt
+const cookieParser = require('cookie-parser') //cookie jwt
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -10,17 +10,21 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
+    origin: 'http://localhost:5173', //jwt
+    credentials: true //jwt
 }));
 app.use(express.json());
+
+//jwt created
 app.use(cookieParser())
+    //////////
 const logger = (req, res, next) => {
-    console.log('inside the logger middlewer')
-    next()
-}
+        console.log('inside the logger middlewer')
+        next()
+    }
+    ///////cookies
 const cookie = (req, res, next) => {
-    const token = req.cookies.token; //  এটা আগে define করো
+    const token = req.cookies.token;
     console.log('cookie in the middleware', token)
 
     if (!token) {
@@ -32,8 +36,8 @@ const cookie = (req, res, next) => {
             return res.status(401).send({ message: 'unauthorized access' });
         }
 
-        req.decoded = decoded; //  decoded user info req.user এ রাখো
-        next(); //  সব কিছু ঠিক থাকলে পরবর্তী middleware বা route এ যাও
+        req.decoded = decoded; //  decoded user info req.user 
+        next();
     });
 };
 
@@ -61,27 +65,30 @@ async function run() {
         app.post('/jwt', async(req, res) => {
             const user = req.body
                 //token created
-            const token = jwt.sign(user, process.env.JWT_SECRET_KEY, {
-                expiresIn: '7d'
-            })
+            const token = jwt.sign({ userEmail: user.email }, process.env.JWT_SECRET_KEY, {
+                expiresIn: '20d'
+            });
+
             console.log(token)
             res.cookie('token', token, {
                 httpOnly: true,
                 secure: false,
                 sameSite: 'Lax',
-
             })
             res.send({ success: true })
         })
 
         //  Get all items or filter by user's email
-        app.get('/items', cookie, async(req, res) => {
-            const userEmail = req.query.email;
-            console.log('insite lost and found', req.cookies)
-            const query = userEmail ? { userEmail } : {};
-            const items = await itemCollection.find(query).toArray();
-            res.send(items);
-        });
+        /*  app.get('/items', cookie, logger, async(req, res) => { //use jwt
+             const userEmail = req.query.email;
+             console.log('insite lost and found', req.cookies) //jwt
+             if (userEmail !== req.decoded.email) {
+                 return res.status(403).send({ massege: 'forbidden access' })
+             }
+             const query = userEmail ? { userEmail } : {};
+             const items = await itemCollection.find(query).toArray();
+             res.send(items);
+         }); */
 
         //  Get latest 6 items for home page
         app.get('/items/home', async(req, res) => {
@@ -97,7 +104,7 @@ async function run() {
         });
 
         //  Add new item
-        app.post('/items', async(req, res) => {
+        app.post('/items', cookie, async(req, res) => {
             const item = req.body;
             const result = await itemCollection.insertOne(item);
             res.send(result);
@@ -133,14 +140,14 @@ async function run() {
         });
 
         // Save recovered item to recoveredCollection
-        app.post('/recovered', async(req, res) => {
+        app.post('/recovered', cookie, async(req, res) => {
             const recoveredData = req.body; // { title, recoveredLocation, recoveredDate, recoveredBy }
             const result = await recoveredCollection.insertOne(recoveredData);
             res.send(result);
         });
 
         //  Get recovered items by user email
-        app.get('/recovered', cookie, async(req, res) => {
+        app.get('/recovered', cookie, async(req, res) => { //use cookies
             const email = req.query.email;
             const query = { "recoveredBy.email": email };
             const items = await recoveredCollection.find(query).toArray();
